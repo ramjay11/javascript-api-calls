e1084a51c70e13f1ad02f5e4b0eecac268b69e44
Ramjay Romorosa <ramjayromorosa@yahoo.com>
2022-06-20 02:23:37 +0800

    added new file and 3 more libraries for api calls

--- XMLHttpRequest.js
+++ XMLHttpRequest.js
@@ -11,3 +11,45 @@ request.onload = () => {
         console.log(`error ${request.status}`)
     }
 }
+
+// Fetch API. Disadvantage of Fetch API is error handling
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => {
+        //console.log(response);
+        return response.json();
+    }).then(json => {
+        console.log(json);
+    })
+
+// Second approach
+async function getUsers() {
+    let response = await fetch('https://jsonplaceholder.typicode.com/users');
+    let data = await response.json();
+    return data;
+}
+
+getUsers().then(response => {
+    console.log(response)
+})
+
+// Axios - third-party javascript library. Promise-based http client
+axios.get('https://jsonplaceholder.typicode.com/users')
+    .then(response => {
+        console.log(response.data);
+    }, err => {
+        console.log(err)
+    })
+
+// JQuery
+$(document).ready(function() {
+    $.ajax({
+        url:'https://jsonplaceholder.typicode.com/users',
+        type:'GET',
+        success:function(result) {
+            console.log(result)
+        }, 
+        error:function() {
+            console.log(err);
+        }
+    })
+})
